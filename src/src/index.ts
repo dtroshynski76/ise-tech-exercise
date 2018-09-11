3fbd9d67d04b1185bec213ab14c59b80264f234b
Donovan Troshynski <dtroshynski1997@gmail.com>
2018-09-10 21:56:03 -0500

    To Do Beginning

---
+++ src/src/index.ts
@@ -0,0 +1,17 @@
+import * as angular from 'angular';
+
+import 'angular-ui-router';
+import routesConfig from './routes';
+
+import {main} from './app/main';
+import {header} from './app/header';
+import {footer} from './app/footer';
+
+import './index.scss';
+
+angular
+  .module('app', ['ui.router'])
+  .config(routesConfig)
+  .component('app', main)
+  .component('fountainHeader', header)
+  .component('fountainFooter', footer);
